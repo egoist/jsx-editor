747a28e2a9686261156c446f6aaef3f8b11823dd
EGOIST <0x142857@gmail.com>
2017-05-23 17:08:08 +0800

    fix config

--- poi.config.js
+++ poi.config.js
@@ -3,20 +3,6 @@ const OfflinePlugin = require('offline-plugin')
 module.exports = options => ({
   // Disable sourceMap in production mode
   sourceMap: options.mode === 'development',
-  extendWebpack(config) {
-    // Enable pwa support in production mode
-    if (options.mode === 'production') {
-      config.plugin('offline')
-        .use(OfflinePlugin, [{
-          ServiceWorker: {
-            events: true
-          },
-          AppCache: {
-            events: true
-          }
-        }])
-    }
-  },
   presets: [
     require('poi-preset-offline')(),
     require('poi-preset-bundle-report')()
