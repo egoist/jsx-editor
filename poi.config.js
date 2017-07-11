fb5b4e7313aa9c50612f6cea9abf1032c799e431
EGOIST <0x142857@gmail.com>
2017-07-11 22:38:08 +0800

    update deps

--- poi.config.js
+++ poi.config.js
@@ -3,6 +3,9 @@ const OfflinePlugin = require('offline-plugin')
 module.exports = options => ({
   // Disable sourceMap in production mode
   sourceMap: options.mode === 'development',
+  extendWebpack(config) {
+    config.module.noParse.add(/babel-standalone/)
+  },
   presets: [
     require('poi-preset-offline')(),
     require('poi-preset-bundle-report')()
